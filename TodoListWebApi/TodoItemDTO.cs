33be7dae36e444375a462ecd9e851bc93b4113f9
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-05-19 23:12:09 +0100

    Add project files.

---
+++ TodoListWebApi/TodoItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListWebApi
+{
+    public class TodoItemDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public TodoItemDTO() { }
+        public TodoItemDTO(Todo todoItem) 
+            => (Id, Name, IsCompleted) = (todoItem.Id, todoItem.Name, todoItem.IsCompleted);
+    }
+}
